b916b0c4e39181df7d7e7cbaad790f6cd74893f8
4nth0ny1 <acatullo4@gmail.com>
2024-03-29 00:50:29 -0500

    added first-blog and second-blog

---
+++ src/app/(blogs)/first-blog/page.tsx
@@ -0,0 +1,5 @@
+export default function FirstBlog() {
+  return (
+    <div className="flex min-h-screen flex-col items-center">First Blog</div>
+  );
+}
---
+++ src/app/(blogs)/second-blog/page.tsx
@@ -0,0 +1,5 @@
+export default function SecondBlog() {
+  return (
+    <div className="flex min-h-screen flex-col items-center">Second Blog</div>
+  );
+}
