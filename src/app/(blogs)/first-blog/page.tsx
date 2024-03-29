899fa6e476cd42b785b483b148bcfd8b8071c2dd
4nth0ny1 <acatullo4@gmail.com>
2024-03-29 16:40:55 -0500

    better toc

--- src/app/(blogs)/first-blog/page.tsx
+++ src/app/(blogs)/first-blog/page.tsx
@@ -6,26 +6,35 @@ import Paragraph from "~/app/_components/blogStyleComponents/Paragraph";
 import Subheading from "~/app/_components/blogStyleComponents/Subheading";
 import Title from "~/app/_components/blogStyleComponents/Title";
 
+const subs = [
+  { sub_one: "This is how we do it" },
+  { sub_two: "best stuff ever" },
+  { sub_three: "Yo Yo" },
+];
+
 export default function FirstBlog() {
   return (
     <div className="flex min-h-screen flex-col items-center">
       <Title title="By order of the Peaky Blinders" />
       <TableOfContents />
       <Paragraph paragraph="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)." />
-      <div id="1">
-        <Subheading subheading="1" />
-      </div>
       <Paragraph paragraph="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)." />
       <Paragraph paragraph="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)." />
       <Paragraph paragraph="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)." />
-      <div id="2">
-        <Subheading id="2" subheading="2" />
-      </div>
+
+      <Subheading id={subs[0]?.sub_one} subheading={subs[0]?.sub_one} />
+
       <Paragraph paragraph="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)." />
       <Paragraph paragraph="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)." />
-      <div id="3">
-        <Subheading subheading="3" />
-      </div>
+      <Paragraph paragraph="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)." />
+
+      <Subheading id={subs[1]?.sub_two} subheading={subs[1]?.sub_two} />
+
+      <Paragraph paragraph="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)." />
+      <Paragraph paragraph="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)." />
+
+      <Subheading id={subs[2]?.sub_three} subheading={subs[2]?.sub_three} />
+
       <Paragraph paragraph="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)." />
       <Paragraph paragraph="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)." />
     </div>
@@ -39,11 +48,17 @@ function scrollYo(hash: string) {
 function TableOfContents() {
   return (
     <div>
-      <button onClick={() => scrollYo("1")}>1</button>
+      <button onClick={() => scrollYo(subs[0]?.sub_one as string)}>
+        {subs[0]?.sub_one}
+      </button>{" "}
       <br></br>
-      <button onClick={() => scrollYo("2")}>2</button>
+      <button onClick={() => scrollYo(subs[1]?.sub_two as string)}>
+        {subs[1]?.sub_two}
+      </button>{" "}
       <br></br>
-      <button onClick={() => scrollYo("3")}>3</button>
+      <button onClick={() => scrollYo(subs[2]?.sub_three as string)}>
+        {subs[2]?.sub_three}
+      </button>{" "}
     </div>
   );
 }
--- src/app/_components/blogStyleComponents/Subheading.tsx
+++ src/app/_components/blogStyleComponents/Subheading.tsx
@@ -1,7 +1,12 @@
 interface SubHeadingProps {
+  id: string;
   subheading: string;
 }
 
-export default function Subheading({ subheading }: SubHeadingProps) {
-  return <h2 className={`pb-2 pt-8 text-3xl ${subheading}`}>{subheading}</h2>;
+export default function Subheading({ id, subheading }: SubHeadingProps) {
+  return (
+    <h2 id={id} className={`pb-2 pt-8 text-3xl ${subheading}`}>
+      {subheading}
+    </h2>
+  );
 }
