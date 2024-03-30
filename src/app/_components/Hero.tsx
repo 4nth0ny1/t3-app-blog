8aa2efb9c42fad6492f4fbd0565900f01c9574d3
4nth0ny1 <acatullo4@gmail.com>
2024-03-30 11:47:38 -0500

    updated hero

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -3,18 +3,19 @@ export default function Hero() {
     <div
       className="hero min-h-screen"
       style={{
-        backgroundImage:
-          "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
+        backgroundImage: "url(/hero-david-goldsbury.jpg)",
       }}
     >
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <h1 className="mb-5 text-5xl font-bold">Golf Mastery</h1>
           <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            Welcome to the internet&apos;s best source for improving your game.
+            At Golf Mastery, we recognize that golf is difficult and takes time
+            and practice to improve. Together, we can disect every facet of your
+            game with articles about the basics, drills, strategies, and buying
+            guides to the industry&apos;s best equipment.
           </p>
           <button className="btn btn-primary">Get Started</button>
         </div>
