d1db7e54660bfa71535c027bf7851259aa8a6a0a
4nth0ny1 <acatullo4@gmail.com>
2024-03-30 11:15:34 -0500

    added Card component and updated blogs page

--- src/app/_components/Card.tsx
+++ src/app/_components/Card.tsx
@@ -1,23 +1,54 @@
-export default function Card() {
+import Image from "next/image";
+import Link from "next/link";
+
+interface CardProps {
+  id: number;
+  imgUrl: string;
+  imgAlt: string;
+  title: string;
+  newBlog: boolean;
+  description: string;
+  tagArray: { id: number; name: string }[];
+  linkUrl: string;
+}
+
+export default function Card({
+  id,
+  imgUrl,
+  imgAlt,
+  title,
+  newBlog,
+  description,
+  tagArray,
+  linkUrl,
+}: CardProps) {
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
-      <figure>
-        <img
-          src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-          alt="Shoes"
-        />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">
-          Shoes!
-          <div className="badge badge-secondary">NEW</div>
-        </h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
+    <Link href={linkUrl}>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure>
+          <Image src={imgUrl} alt={imgAlt} width={300} height={300} />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">
+            {title}
+            {newBlog && (
+              <div className="badge badge-secondary">{newBlog && "NEW"}</div>
+            )}
+          </h2>
+          <p>{description}</p>
+          {tagArray?.map((tag) => {
+            return (
+              <div className="card-actions justify-end" key={tag.id}>
+                <div className="badge badge-outline">{tag.name}</div>
+              </div>
+            );
+          })}
+          {/* <div className="card-actions justify-end">
           <div className="badge badge-outline">Fashion</div>
           <div className="badge badge-outline">Products</div>
+        </div> */}
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,19 +1,54 @@
 import Card from "../_components/Card";
 import BlogWrapper from "../_components/blogStyleComponents/BlogWrapper";
 
+const blogArray = [
+  {
+    id: 1,
+    imgUrl: "/chipping-matt-seymour.jpg",
+    imgAlt: "chipping-image",
+    title: "first blog",
+    newBlog: true,
+    description: "This is the description",
+    tagArray: [
+      { id: 1, name: "wedges" },
+      { id: 2, name: "short game" },
+    ],
+    linkUrl: "/first-blog",
+  },
+  {
+    id: 2,
+    imgUrl: "/putting-richard-stott.jpg",
+    imgAlt: "putting-image",
+    title: "second blog",
+    newBlog: false,
+    description: "This is the description",
+    tagArray: [
+      { id: 1, name: "wedges" },
+      { id: 2, name: "short game" },
+    ],
+    linkUrl: "/second-blog",
+  },
+];
+
 export default function BlogsPage() {
   return (
     <BlogWrapper>
       <h1 className="py-12 text-center text-4xl">Blogs</h1>
       <div className="flex shrink flex-wrap gap-6">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {blogArray.map((blog) => {
+          return (
+            <Card
+              id={blog.id}
+              imgUrl={blog.imgUrl}
+              imgAlt={blog.imgAlt}
+              title={blog.title}
+              newBlog={blog.newBlog}
+              description={blog.description}
+              tagArray={blog.tagArray}
+              linkUrl={blog.linkUrl}
+            />
+          );
+        })}
       </div>
     </BlogWrapper>
   );
