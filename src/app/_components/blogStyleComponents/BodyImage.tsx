7cd8c5c15b0936ff2fb3a5954030526fae39e625
4nth0ny1 <acatullo4@gmail.com>
2024-04-02 17:24:48 -0500

    updated the img tags to Image

--- src/app/_components/blogStyleComponents/BodyImage.tsx
+++ src/app/_components/blogStyleComponents/BodyImage.tsx
@@ -1,7 +1,17 @@
+import Image from "next/link";
+
 interface BodyImageProps {
   srcLink: string;
 }
 
 export default function BodyImage({ srcLink }: BodyImageProps) {
-  return <img className="px-32 py-12" src={srcLink} />;
+  return (
+    <Image
+      width={50}
+      height={50}
+      className="px-32 py-12"
+      src={srcLink}
+      alt=""
+    />
+  );
 }
--- src/app/_components/blogStyleComponents/HeaderImage.tsx
+++ src/app/_components/blogStyleComponents/HeaderImage.tsx
@@ -1,7 +1,11 @@
+import Image from "next/link";
+
 interface HeaderImageProps {
   srcLink: string;
 }
 
 export default function HeaderImage({ srcLink }: HeaderImageProps) {
-  return <img className="py-12" src={srcLink} />;
+  return (
+    <Image width={1500} height={1500} alt="" className="py-12" src={srcLink} />
+  );
 }
