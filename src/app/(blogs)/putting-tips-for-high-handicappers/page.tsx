c99bdb1581832bea7576abed9cb460151ba3ff25
4nth0ny1 <acatullo4@gmail.com>
2024-04-03 18:03:56 -0500

    added more content and top padding for H3

--- src/app/(blogs)/putting-tips-for-high-handicappers/page.tsx
+++ src/app/(blogs)/putting-tips-for-high-handicappers/page.tsx
@@ -6,7 +6,7 @@ import Paragraph from "~/app/_components/blogStyleComponents/Paragraph";
 import Subheading from "~/app/_components/blogStyleComponents/Subheading";
 import TableOfContents from "~/app/_components/blogStyleComponents/TableOfContents";
 import Title from "~/app/_components/blogStyleComponents/Title";
-import H3Heading from "~/app/_components/blogStyleComponents/h3Heading";
+import H3Heading from "~/app/_components/blogStyleComponents/H3Heading";
 
 const subs = [
   { id: 1, sub: "Part 1 - Fundamentals " },
@@ -59,7 +59,26 @@ export default function PuttingTipsForHighHandicappers() {
           <Paragraph paragraph="Many teachers will tell you that you should try to get your put to have the speed that can go 12-18 inches past the hole. I disagree with this especially during practice. What helped me tremendously, was trying to be as specific as possible. With  putting, “aim small, miss small” is the mantra I recite when I decide how to putt. " />
           <Subheading id={subs[1]?.sub} subheading={subs[1]?.sub} />
           <Paragraph paragraph="If you really want to get good at golf, practicing needs to be just as fun or more fun than playing itself. The drills below are meant to be done the way they are. These drills helped me quite a bit. Focus on results, if you feel that you’re lacking in an area then practice that area more. If you’re conscious enough of something that you’re not doing well, then you’re halfway there. All you have to do is execute and get results. " />
+          <H3Heading heading="Three balls (practice almost everywhere)" />
+          <Paragraph paragraph="Get three balls and draw a big vertical line on it. Every time you hit a put, you should use this line to point to the exact point you want to hit. " />
+          <Paragraph paragraph="Start from 3ft from the hole and make all three. When you make all three move back 1ft. Then repeat. If you miss one of them move forward 1ft and if you make all three move back. " />
+          <Paragraph paragraph="What you’re trying to do is find feel and a little bit of green reading. After a few times of practicing this drill, you’ll be able to make 3 balls at distances you won’t believe. " />
+          <Paragraph paragraph="The repetitive nature of this drill is what will improve your stroke and make it very consistent. You’ll also be able to confidently line up and make these puts. It will help you solidify your setup too. " />
+          <H3Heading heading="More advanced version" />
+          <Paragraph paragraph="Start from 3ft and the same rules apply, however you’re not trying to just make these balls. This time you only want the ball to go in on one side of the cup. It during this drill I will aim for a blade of grass on one side of the hole and try to make the ball stop on that particular blade. " />
+          <Paragraph paragraph="If you don’t have a green to practice on or the weather is bad, you can do this at home and aim for the corners of furniture or anything that is really small. Remember, try and make the ball, just tap the spot barely making a sound. " />
+          <Paragraph
+            paragraph="These next 2 drills need to be done on a practice green with long puts
+Randomize putting with one ball
+"
+          />
+          <Paragraph
+            paragraph="Long puts only 2ft and in.
+Fight against three puts and assume you will miss so that your next put is on the right side of the hole. 
+"
+          />
           <Subheading id={subs[2]?.sub} subheading={subs[2]?.sub} />
+
           <Subheading id={subs[3]?.sub} subheading={subs[3]?.sub} />
         </div>
       </div>
--- src/app/_components/blogStyleComponents/H3Heading.tsx
+++ src/app/_components/blogStyleComponents/H3Heading.tsx
@@ -3,5 +3,5 @@ interface H3HeadingProps {
 }
 
 export default function H3Heading({ heading }: H3HeadingProps) {
-  return <h3 className="text-2xl italic">{heading}</h3>;
+  return <h3 className="pt-6 text-2xl italic">{heading}</h3>;
 }
