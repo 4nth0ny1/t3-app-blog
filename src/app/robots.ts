233fe564bbdc504a11cc20761c44a7c5c67a7229
4nth0ny1 <acatullo4@gmail.com>
2024-04-04 17:19:40 -0500

    added sitemap and robots skeleton, more work still needed

---
+++ src/app/robots.ts
@@ -0,0 +1,14 @@
+// import type { MetadataRoute } from "next";
+
+// export default function robots(): MetadataRoute.Robots {
+//   return {
+//     rules: {
+//       userAgent: "*",
+//       allow: "/", // allows all pages to be crawled by google
+//       disallow: ["/admin", "/private/"], // disallows certain pages to be crawled by google
+//     },
+//     sitemap: "https://acme.com/sitemap.xml", // TODO: change when domain is ready
+//   };
+// }
+
+// // you can also go the file of exact page and add robots field to the metadata hash to allow crawler to follow the links and then not index the page.
---
+++ src/app/sitemap.ts
@@ -0,0 +1,33 @@
+/* 
+  
+  TODO: WAIT UNTIL THE URL IS SELECTED. 
+
+  DONT USE THE new Date(), because Google will ignore this. Use the actual date when you modify it. 
+  Change freq and priority dont need to be used either AS I KNOW NOW.
+
+*/
+
+// import type { MetadataRoute } from "next";
+
+// export default function sitemap(): MetadataRoute.Sitemap {
+//   return [
+//     {
+//       url: "https://acme.com",
+//       // lastModified: new Date(),
+//       // changeFrequency: "yearly",
+//       // priority: 1,
+//     },
+//     {
+//       url: "https://acme.com/about",
+//       // lastModified: new Date(),
+//       // changeFrequency: "monthly",
+//       // priority: 0.8,
+//     },
+//     {
+//       url: "https://acme.com/blog",
+//       // lastModified: new Date(),
+//       // changeFrequency: "weekly",
+//       // priority: 0.5,
+//     },
+//   ];
+// }
